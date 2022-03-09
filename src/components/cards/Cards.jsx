a8e89bd98bd593b18c50057a4f7f2459f2f813ed
fatihozkul35 <fatihozkul3541@gmail.com>
2022-03-09 20:45:52 +0100

    last version 1.0

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -6,24 +6,21 @@ const Main = () => {
       <h1>POPULAR TOUR PLACES</h1>
       <div className="cards">
         {data.map((card) => {
-          const { id, title, image, desc } = card;
           return (
-            <div key={id} className="card">
+            <div key={card.id} className="card">
               <div className="card-body">
-                <h5 className="card-title">{title}</h5>
+                <h5 className="card-title">{card.title}</h5>
               </div>
               <div
                 className="img"
                 style={{ width: "18rem", height: "10.5rem" }}
               >
                 <img
-                  src={image}
+                  src={card.image}
                   alt="here is a fhoto of any country"
                   className="card-img-top"
                 />
-                <p className="desc">{desc}</p>
               </div>
-              {/* <div className="desc">{desc}</div> */}
             </div>
           );
         })}
